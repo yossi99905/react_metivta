e98547169d90f3a9c0ff3a24cc9c43e4aa867a0e
yossi mizrachi <yossimizrach.770@gmail.com>
2024-05-08 22:39:25 +0300

    Update DashboardAdmin.jsx and EditUser.jsx components

--- src/components/admin/DashboardAdmin.jsx
+++ src/components/admin/DashboardAdmin.jsx
@@ -1,48 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavAdmin from './NavAdmin'
-import CircleDesign from '../CircleDesign'
-import HeaderAdmin from './HeaderAdmin'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faDollarSign,faCartShopping, faWallet } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faDollarSign, faCartShopping, faWallet } from '@fortawesome/free-solid-svg-icons'
+import useNum from '../../hook/useNum'
 
 function DashboardAdmin() {
+    const { num: num1, start: start1 } = useNum({ autoplay: true, score: 1256 });
+    const { num: num2, start: start2 } = useNum({ autoplay: true, score: 26 });
+    const { num: num3, start: start3 } = useNum({ autoplay: true, score: 7 });
+    const { num: num4, start: start4 } = useNum({ autoplay: true, score: 122 });
     
+    useEffect(() => {
+        start1()
+        start2()
+        start3()
+        start4()
+    } , [])
+
     return (
 
         <div className='flex h-lvh justify-end'>
             <div className='w-lvw '>
 
-             
+
                 <div className="mx-9 grid grid-cols-2 gap-4 my-32 h-[500px]">
                     <div className="border p-4 text-center flex flex-col justify-center items-center space-y-4">
                         <div className='flex justify-center items-center space-x-4'>
 
-                            <FontAwesomeIcon className='size-32 text-tailwind-green' icon={faUser} />
-                            <p className='font-bold text-tailwind-green text-[120px] my-auto'>26</p>
+                            <FontAwesomeIcon className='size-20 text-tailwind-green' icon={faUser} />
+                            <p className='font-bold text-tailwind-green text-[85px] my-auto'>{num1}</p>
                         </div>
                         <p>מספר משתמשים קיימים</p>
                     </div>
                     <div className="border p-4 text-center flex flex-col justify-center items-center space-y-4">
                         <div className='flex justify-center items-center space-x-4'>
 
-                            <FontAwesomeIcon className='size-32 text-tailwind-green' icon={faDollarSign} />
-                            <p className='font-bold text-tailwind-green text-[120px] my-auto'>1256</p>
+                            <FontAwesomeIcon className='size-20 text-tailwind-green' icon={faDollarSign} />
+                            <p className='font-bold text-tailwind-green text-[85px] my-auto'>{num2}</p>
                         </div>
                         <p>מספר נקודות במערכת</p>
                     </div>
                     <div className="border p-4 text-center flex flex-col justify-center items-center space-y-4">
                         <div className='flex justify-center items-center space-x-4'>
 
-                            <FontAwesomeIcon className='size-32 text-tailwind-green' icon={faCartShopping} />
-                            <p className='font-bold text-tailwind-green text-[120px] my-auto'>7</p>
+                            <FontAwesomeIcon className='size-20 text-tailwind-green' icon={faCartShopping} />
+                            <p className='font-bold text-tailwind-green text-[85px] my-auto'>{num3}</p>
                         </div>
                         <p>מספר רכישות שבוצעו היום</p>
                     </div>
                     <div className="border p-4 text-center flex flex-col justify-center items-center space-y-4">
                         <div className='flex justify-center items-center space-x-4'>
 
-                            <FontAwesomeIcon className='size-24 text-tailwind-green' icon={faWallet} />
-                            <p className='font-bold text-tailwind-green text-[120px] my-auto'>122</p>
+                            <FontAwesomeIcon className='size-20 text-tailwind-green' icon={faWallet} />
+                            <p className='font-bold text-tailwind-green text-[85px] my-auto'>{num4}</p>
                         </div>
                         <p>מספר נקודות שחולקו היום</p>
                     </div>
--- src/components/admin/EditUser.jsx
+++ src/components/admin/EditUser.jsx
@@ -14,9 +14,9 @@ function EditUser({ showEditForm = false, onClickCloseBtn, firstName,lastName,_i
             <div className='flex justify-center items-center max-w-[500px] m-auto rounded-lg mt-28  '>
                 <form onSubmit={handleSubmit(onClickEdit)} className='bg-tailwind-cream px-7 flex flex-col  space-y-2 text-center  py-6 w-full rounded-lg'>
                     <button className=' text-start ml-10' onClick={onClickCloseBtn}>X</button>
-                    <label htmlFor="">שם התלמיד</label>
+                    <label htmlFor="">שם פרטי</label>
                     <input defaultValue={firstName} {...register("firstName", { required: true, minLength: 2 })} type="text" placeholder="הזן שם פרטי" className='rounded-2xl p-2 text-right w-full' />
-                    <label htmlFor="">שם התלמיד</label>
+                    <label htmlFor="">שם משפחה</label>
                     <input defaultValue={lastName} {...register("lastName", { required: true, minLength: 2 })} type="text" placeholder="הזן שם משפחה" className='rounded-2xl p-2 text-right w-full' />
                     <label htmlFor="">איימל</label>
                     <input defaultValue={email} {...register("email", { required: true, minLength: 4 })} type="text" placeholder="הזן איימל" className='rounded-2xl p-2 text-right w-full' />
@@ -28,7 +28,7 @@ function EditUser({ showEditForm = false, onClickCloseBtn, firstName,lastName,_i
                         <option value="4000">מנהל</option>
                     </select>
                     <label htmlFor="">כיתה</label>
-                    <input defaultValue={classRoom} {...register("classRoom", { required: true, min: 1 })} type="text" placeholder="הזן כיתה" className='rounded-2xl p-2 text-right w-full' />
+                    <input defaultValue={classRoom} {...register("classRoom", { required: true, min: 0 })} type="text" placeholder="הזן כיתה" className='rounded-2xl p-2 text-right w-full' />
                     <label htmlFor="">ת.ז</label>
                     <input defaultValue={ID} {...register("ID", { required: true, minLength: 2 })} type="text" placeholder="הזן תעודת זהות" className='rounded-2xl p-2 text-right w-full' />
                     <label htmlFor="">ניקוד</label>
